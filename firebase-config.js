90a4dc33ba226986c262f857060f1c820c05b218
PangPiNas <badhalloween@gmail.com>
2026-03-16 11:24:41 +0700

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,36 @@
+const firebaseConfig = {
+apiKey: "AIzaSyDw7Fa7nhtJCXx-aUT_iJDpMQMKxbDKrhI",
+authDomain: "school-attendance-24eda.firebaseapp.com",
+projectId: "school-attendance-24eda",
+storageBucket: "school-attendance-24eda.firebasestorage.app",
+messagingSenderId: "624706301213",
+appId: "1:624706301213:web:16d552080d727b44b685f4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+/*
+const db = firebase.firestore();
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDw7Fa7nhtJCXx-aUT_iJDpMQMKxbDKrhI",
+  authDomain: "school-attendance-24eda.firebaseapp.com",
+  projectId: "school-attendance-24eda",
+  storageBucket: "school-attendance-24eda.firebasestorage.app",
+  messagingSenderId: "624706301213",
+  appId: "1:624706301213:web:16d552080d727b44b685f4",
+  measurementId: "G-MW47YP6J33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+*/
